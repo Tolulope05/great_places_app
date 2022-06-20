1f219832793352d629c220a16a643566a37589b7
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-06-20 03:46:18 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/great_places.dart';
+
+import './screens/places_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: ((context) => GreatPlaces()),
+      child: MaterialApp(
+        title: 'Great Places',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          canvasColor: const Color(0xFFE3F2FD),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)
+              .copyWith(secondary: Colors.amber),
+        ),
+        home: const PlacesListScreen(),
+      ),
+    );
+  }
+}
